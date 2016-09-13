3603317bf4b411aaced2ba2c4fe2b8cc50d28df7
Joon Hyub Lee <joonhyub.lee@gmail.com>
2016-09-13 11:42:10 +0900

    use babel to build

--- index.js
+++ index.js
@@ -1,66 +1,76 @@
-module.exports.mock_api = return_arg => {
-    const _calls = [];
-    let count = -1;
+'use strict';
 
-    const proxy = new Proxy(function () {}, {
-        apply(target, this_arg, _args) {
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var mock_api = exports.mock_api = function mock_api(return_arg) {
+    var _calls = [];
+    var count = -1;
+
+    var proxy = new Proxy(function () {}, {
+        apply: function apply(target, this_arg, _args) {
             // function called initially
-            _calls.push({ _args });
+            _calls.push({ _args: _args });
             count++;
             return make_sub_proxy(count);
         },
-
-        get(target, key) {
+        get: function get(target, key) {
             // get _calls
             if (key == '_calls') return _calls;
 
             // await foo().bar() will resolve to return_arg
-            else if (key == 'then') return resolve => resolve(return_arg);
+            else if (key == 'then') return function (resolve) {
+                    return resolve(return_arg);
+                };
 
-            // property accessed initially
-            else {
-                _calls.push({ [key]: {} });
-                count++;
-                return make_sub_proxy(count);
-            }
+                // property accessed initially
+                else {
+                        _calls.push(_defineProperty({}, key, {}));
+                        count++;
+                        return make_sub_proxy(count);
+                    }
         }
     });
 
-    const make_sub_proxy = count => {
+    var make_sub_proxy = function make_sub_proxy(count) {
 
         // traverse to the current position in call_list
-        let temp = _calls[count];
-        for (let key in temp) {
+        var temp = _calls[count];
+        for (var key in temp) {
             if (key != '_args') temp = temp[key];
         }
 
-        const sub_proxy = new Proxy(function () {}, {
-            apply(target, this_arg, _args) {
+        var sub_proxy = new Proxy(function () {}, {
+            apply: function apply(target, this_arg, _args) {
                 // function called
                 // foo('a','b','c')
                 if (!temp.hasOwnProperty('_args')) temp._args = _args;
 
                 // foo('a','b','c')(1,2,3)
                 else {
-                    temp._anonymous = { _args };
-                    temp = temp._anonymous;
-                }
+                        temp._anonymous = { _args: _args };
+                        temp = temp._anonymous;
+                    }
                 return sub_proxy;
             },
-
-            get(target, key) {
+            get: function get(target, key) {
                 // get _calls
                 if (key == '_calls') return _calls;
 
                 // await foo().bar() will resolve to return_arg
-                else if (key == 'then') return resolve => resolve(return_arg);
+                else if (key == 'then') return function (resolve) {
+                        return resolve(return_arg);
+                    };
 
-                // property accessed
-                else {
-                    temp[key] = {};
-                    temp = temp[key];
-                    return sub_proxy;
-                }
+                    // property accessed
+                    else {
+                            temp[key] = {};
+                            temp = temp[key];
+                            return sub_proxy;
+                        }
             }
         });
 
@@ -71,4 +81,6 @@ module.exports.mock_api = return_arg => {
 };
 
 // call_fn: api => api.foo().bar()
-module.exports.mock_call = call_fn => call_fn(mock_api())._calls[0];
+var mock_call = exports.mock_call = function mock_call(call_fn) {
+    return call_fn(mock_api())._calls[0];
+};
