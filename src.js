5cb951eb6f6395b035e3334b5305869d4485b6cf
Joon Hyub Lee <joonhyub.lee@gmail.com>
2016-09-13 13:10:01 +0900

    remove count variable

--- src.js
+++ src.js
@@ -1,13 +1,12 @@
 export const mock_api = return_arg => {
     const _calls = [];
-    let count = -1;
 
     const proxy = new Proxy(function () {}, {
         apply(target, this_arg, _args) {
             // function called initially
-            _calls.push({ _args });
-            count++;
-            return make_sub_proxy(count);
+            const call = { _args }
+            _calls.push(call);
+            return make_sub_proxy(call);
         },
 
         get(target, key) {
@@ -19,17 +18,17 @@ export const mock_api = return_arg => {
 
             // property accessed initially
             else {
-                _calls.push({ [key]: {} });
-                count++;
-                return make_sub_proxy(count);
+                const call = { [key]: {} };
+                _calls.push(call);
+                return make_sub_proxy(call);
             }
         }
     });
 
-    const make_sub_proxy = count => {
+    const make_sub_proxy = call => {
 
         // traverse to the current position in call_list
-        let temp = _calls[count];
+        let temp = call;
         for (let key in temp) {
             if (key != '_args') temp = temp[key];
         }
@@ -40,6 +39,7 @@ export const mock_api = return_arg => {
                 // foo('a','b','c')
                 if (!temp.hasOwnProperty('_args')) temp._args = _args;
 
+                // function called without explicit name
                 // foo('a','b','c')(1,2,3)
                 else {
                     temp._anonymous = { _args };
